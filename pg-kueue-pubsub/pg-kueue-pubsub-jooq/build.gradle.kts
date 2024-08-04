29e972fb5e3555d8081a218877bff1d64eb46c51
vooft <52610+vooft@users.noreply.github.com>
2024-08-04 19:07:19 +0400

    Refactor to have separate modules for transport and pubsub

---
+++ pg-kueue-pubsub/pg-kueue-pubsub-jooq/build.gradle.kts
@@ -0,0 +1,17 @@
+dependencies {
+    api(project(":pg-kueue-pubsub:pg-kueue-pubsub-core"))
+    implementation(project(":pg-kueue-pubsub:pg-kueue-pubsub-jdbc"))
+    implementation(project(":pg-kueue-transport:pg-kueue-transport-jdbc"))
+    implementation(project(":pg-kueue-utils"))
+
+    compileOnly(libs.pg.jdbc)
+    compileOnly(libs.jooq)
+
+    testImplementation(testFixtures(project(":pg-kueue-utils")))
+    testImplementation(libs.bundles.junit)
+    testImplementation(libs.bundles.kotest)
+    testImplementation(libs.pg.jdbc)
+    testImplementation(libs.jooq)
+    testImplementation(libs.slf4j.simple)
+    testImplementation(libs.hikaricp)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,9 +8,11 @@ include(
 
     ":pg-kueue-pubsub:pg-kueue-pubsub-core",
     ":pg-kueue-pubsub:pg-kueue-pubsub-jdbc",
+    ":pg-kueue-pubsub:pg-kueue-pubsub-jdbc-test",
+    ":pg-kueue-pubsub:pg-kueue-pubsub-jooq",
+    ":pg-kueue-pubsub:pg-kueue-pubsub-jooq-test",
 
     ":pg-kueue-transport:pg-kueue-transport-core",
     ":pg-kueue-transport:pg-kueue-transport-jdbc",
-    ":pg-kueue-transport:pg-kueue-transport-jdbc-test",
 )
 
